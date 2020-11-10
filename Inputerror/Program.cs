b556036d01dadc2e30cc315c55dbc32ec4a6b816
sohas <sv1311@gmail.com>
2020-11-10 21:23:04 +0300

    Добавьте файлы проекта.

---
+++ Inputerror/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Inputerror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("введите текст и нажмите enter");
+                Console.WriteLine();
+                var text = Console.ReadLine();
+                Console.WriteLine();
+                foreach (var ch in text)
+                    Console.Write(EngToRuss.GetChar(ch));
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
